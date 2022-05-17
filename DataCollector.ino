6ce8905f9731aaaa4cb9b35b8b9bda3ab45923a9
CrazyCipherGamer5827 <raja.sell5827@gmail.com>
2022-05-17 10:53:20 -0700

    Fix errors in reading data.

--- DataCollector.ino
+++ DataCollector.ino
@@ -1,11 +1,12 @@
 //Author: Paramveer Singh
 //Created on: 7 March 2022
+//Updated on: 17 May 2022
 //Description: This program simply polls the EC and RTD sensors 10 times
 //             and puts the readings into a 10 element float buffer for each sensor.
 
 #include <Ezo_i2c.h>  //include the EZO I2C library from https://github.com/Atlas-Scientific/Ezo_I2c_lib
 #include <Wire.h>     //include library for activating Arduino's I2C.
-#include <sequencer2.h> //imports a 2 function sequencer
+#include <Ezo_i2c_util.h> //brings in common print statments
 
 Ezo_board EC = Ezo_board(100, "EC");
 Ezo_board RTD = Ezo_board(102, "RTD");
@@ -14,21 +15,20 @@ float EC_data[10];
 float RTD_data[10];
 int elementNum;
 
-void step1();
-void step2();
+void SendReadCmds();
+void ReadReadings();
 
-Sequencer2 Seq(&step1, 1000,
-               &step2, 0);
 
 void setup() {
   Serial.begin(9600); //Start serial port
   Wire.begin(); //start i2c
-  Seq.reset();  //initialize sequencer
-  delay(10000);
-  
-  for(elementNum = 0; elementNum < 10; ++elementNum)
+  delay(3000);
+  for(elementNum = 0; elementNum < 10; elementNum++)
   {
-    Seq.run();
+    SendReadCmds();
+    delay(1000);
+    ReadReadings();
+    delay(100);
   }
 
   Serial.println("10 data points received");
@@ -46,27 +46,30 @@ void setup() {
     Serial.print(stringToPrint + " ");
   }
   Serial.print("\n");
+  
 }
 
 void loop() {
 }
 
-void step1()
+void SendReadCmds()
 {
   RTD.send_read_cmd();
   EC.send_read_cmd();
 }
 
-void step2()
+void ReadReadings()
 {
-  if(RTD.get_error() == Ezo_board::SUCCESS && RTD.get_last_received_reading() > -1000.0)    //Check if the data is valid or not
+  RTD.receive_read_cmd();
+  if(RTD.get_error() == Ezo_board::SUCCESS)    //Check if the data is valid or not
   {
-     RTD_data[elementNum] = RTD.get_last_received_reading();
+    float reading = RTD.get_last_received_reading();
+    RTD_data[elementNum] = reading;
   }
   else
   {
     RTD_data[elementNum] = 25.0f;
   }
-
+  EC.receive_read_cmd();
   EC_data[elementNum] = EC.get_last_received_reading();
 }
